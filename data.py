ca9ec83c8265827979bba79019a03ae3cbb2d815
danlanko <dalanko1@gmail.com>
2017-10-06 07:49:47 +0100

    FLASKAPP

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'title':'Article One',
+            'body':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias asperiores, beatae cum delectus dolor earum ex fuga ipsa laudantium modi nostrum praesentium, provident quis sit voluptates. Aut dolorem recusandae ut!',
+            'author':'Kilanko Daniel',
+            'create_date':'04-10-2017',
+        },
+        {
+            'id':2 ,
+            'title': 'Article 2',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias asperiores, beatae cum delectus dolor earum ex fuga ipsa laudantium modi nostrum praesentium, provident quis sit voluptates. Aut dolorem recusandae ut!',
+            'author': 'Tosin K',
+            'create_date': '04-10-2017',
+        },
+        {
+            'id': 3,
+            'title': 'Article 3',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias asperiores, beatae cum delectus dolor earum ex fuga ipsa laudantium modi nostrum praesentium, provident quis sit voluptates. Aut dolorem recusandae ut!',
+            'author': 'Chuks',
+            'create_date': '04-10-2017',
+        }
+    ]
+
+    return articles
